15c8eb1e09945a67393a70ee6b903ec19a0cedde
ardammr <arda.mamur@voids.ai>
2023-06-06 01:23:13 +0200

    added blogwidgets to homepage

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -7,6 +7,7 @@ import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import AdvertWidget from "scenes/widgets/AdvertWidget";
 import FriendListWidget from "scenes/widgets/FriendListWidget";
+import MergeBlogWidget from "scenes/widgets/MergeBlogWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -34,6 +35,8 @@ const HomePage = () => {
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
+            <MergeBlogWidget />
+            <Box m="2rem 0" />
             <AdvertWidget />
             <Box m="2rem 0" />
             <FriendListWidget userId={_id} />
