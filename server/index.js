9e52c39c6fd85ba23a51cac431bfd9155f289e34
Turker Koc <turker.koc@tum.de>
2023-06-14 23:01:48 +0200

    server index.js fixed

--- server/index.js
+++ server/index.js
@@ -24,59 +24,77 @@ import { verifyToken } from "./middleware/auth.js";
 import { mergeRegister } from "./controllers/mergeAuth.js"; // for registering user
 import { createMergePost } from "./controllers/mergePosts.js";
 
+import User from "./models/User.js"; // for one time user creation
+import Post from "./models/Post.js"; // for one time post creation
+import MergeUser from "./models/MergeUser.js";
+import Location from "./models/Location.js";
+import Price from "./models/Price.js";
+import Category from "./models/Category.js";
+import { mergeUsers, users, posts, locations, prices, categories } from "./data/index.js"; // for one time user and post creation
+
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url); // get current file path
 const __dirname = path.dirname(__filename); // get directory name of current file path
 dotenv.config(); // initialize dotenv
 const app = express(); // initialize express
+app.use(express.json()); // allow us to parse json
 app.use(helmet()); // allow us to set security headers
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // allow us to set security headers, cross origin resource sharing
 app.use(morgan("common")); // allow us to log requests
+app.use(bodyParser.json({ limit: "30mb", extended: true })); // allow us to parse request body
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); // allow us to parse request body with 30mb limit
 app.use(cors()); // allow us to enable cors
 app.use("/assets", express.static(path.join(__dirname, "public/assets"))); // allow us to serve static files (local storage but can bu converted to S3)
 
 /* FILE STORAGE */
-const storage = multer.diskStorage({
+//all below file code taken from multer github repo -> when someone uploads a photo this will set the destination and filename which is public/assets
+const storage = multer.diskStorage({ 
   destination: function (req, file, cb) {
-    cb(null, "public/assets");
+    cb(null, "public/assets"); // set destination to public/assets
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname); // set filename to original filename
   },
 });
-const upload = multer({ storage }); 
+const upload = multer({ storage }); // initialize multer with storage -> we will use this variable to upload files
 
 /* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
-app.post("/mergeAuth/register", upload.single("picture"), mergeRegister);
-app.post("/mergePosts", upload.single("picture"), createMergePost);
+//upload.single("picture") -> if you set picture it will be set in http request body as picture and multer will upload it to public/assets
+app.post("/auth/register", upload.single("picture"), register); // only the register route is defined here because it is the only route that needs to upload a file
+app.post("/posts", verifyToken, upload.single("picture"), createPost); // also this one has a file upload
+app.post("/mergeAuth/register", upload.single("picture"), mergeRegister); // only the register route is defined here because it is the only route that needs to upload a file
+app.post("/mergePosts", upload.single("picture"), createMergePost); // also this one has a file upload
 
 /* STRIPE ROUTE */
 app.use("/stripe", stripeRoutes); // stripe route for payment
 
-/* BODY PARSER */
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
-/* OTHER ROUTES */
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
+/* ROUTES */
+app.use("/auth", authRoutes); // auth route for login and register
+app.use("/users", userRoutes); // user route for getting users
+app.use("/posts", postRoutes); // post route for getting posts
 
 /* MERGE ROUTES */
-app.use("/mergeUsers", mergeUserRoutes);
-app.use("/mergeAuth", mergeAuthRoutes);
-app.use("/mergePosts", mergePostRoutes);
+app.use("/mergeUsers", mergeUserRoutes); // user route for getting users
+app.use("/mergeAuth", mergeAuthRoutes); // auth route for login and register
+app.use("/mergePosts", mergePostRoutes); // post route for getting posts
+
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 6001; // set port from environment variable or 6001
 mongoose
   .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useNewUrlParser: true, // allow us to parse connection string
+    useUnifiedTopology: true, // allow us to use new server discovery and monitoring engine
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`)); // listen to port after connection is established
+
+    /* ADD DUMMY DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+    // MergeUser.insertMany(mergeUsers);
+    // Category.insertMany(categories);
+    // Location.insertMany(locations);
+    // Price.insertMany(prices);    
   })
-  .catch((error) => console.log(`${error} did not connect properly`));
+  .catch((error) => console.log(`${error} did not connect`)); // catch error if connection is not established
