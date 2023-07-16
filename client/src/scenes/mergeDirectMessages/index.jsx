66e23fef2428af5bb59c761e4b1c23a53fea4054
emreboz <emre.boz@tum.de>
2023-07-17 00:33:15 +0200

    draft direction on direct messages

--- client/src/scenes/mergeDirectMessages/index.jsx
+++ client/src/scenes/mergeDirectMessages/index.jsx
@@ -1,14 +1,12 @@
-import { Box, useMediaQuery, Button, useTheme, Select, MenuItem } from "@mui/material";
+import {Box, useMediaQuery, Button, useTheme, Select, MenuItem} from "@mui/material";
 import {useSelector} from "react-redux";
 import {useParams} from "react-router-dom";
 import Navbar from "scenes/navbar";
 import PersonalNavigator from "scenes/widgets/PersonalNavigatorWidget";
 import LinksWidget from "scenes/widgets/LinksWidget";
 import UserCard from "scenes/widgets/UserCardWidget";
-import SummaryWidget from "scenes/widgets/SummaryWidget";
 import ChatWidget from "../widgets/ChatWidget";
-import {useContext} from "react";
-// import {ChatContext} from "../../components/ChatContext";
+import {useNavigate} from "react-router-dom";
 
 const MergeDirectMessages = () => {
     console.log("MergeDirectMessages")
@@ -17,6 +15,7 @@ const MergeDirectMessages = () => {
     const loggedInUser = useSelector((state) => state.user);
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
     let myProfile = false;
+    const navigate = useNavigate();
     if (loggedInUser._id === userId) {
         myProfile = true;
     }
@@ -24,6 +23,11 @@ const MergeDirectMessages = () => {
     console.log("userId:", userId);
 
     if (!userId) return null;
+
+    const handleMyDraftsClick = () => {
+        navigate(`/mergeProfilePage/${userId}?showMyDrafts=true`);
+    };
+
 
     return (
         <Box>
@@ -35,44 +39,47 @@ const MergeDirectMessages = () => {
                 gap="2rem"
             >
                 {isNonMobileScreens && (
-                <Box
-                    flexBasis={isNonMobileScreens ? "26%" : undefined}
-                    paddingLeft="2rem"
-                >
-                <LinksWidget/>
-                    <Box m="2rem 0" />
-                    {myProfile && <PersonalNavigator/>} {/* Conditionally render PersonalNavigator */}
-                </Box>
+                    <Box
+                        flexBasis={isNonMobileScreens ? "26%" : undefined}
+                        paddingLeft="2rem"
+                    >
+                        <LinksWidget/>
+                        <Box m="2rem 0"/>
+                        {myProfile && (
+                            <PersonalNavigator
+                                onMyDraftsClick={handleMyDraftsClick} // Pass the click handler function
+                            />
+                        )}
+                        {/* Conditionally render PersonalNavigator */}
+                    </Box>
                 )}
-                
+
                 {!isNonMobileScreens && (
-                <Box flexBasis="100%" ml='2rem' mr="2rem">
-                    <ChatWidget postUserId={postUserId}/>
-                    <Box m="2rem 0" />
-                    {myProfile && <PersonalNavigator/>} {/* Conditionally render PersonalNavigator */}
-                    <Box m="2rem 0" />
-                </Box>
+                    <Box flexBasis="100%" ml='2rem' mr="2rem">
+                        <ChatWidget postUserId={postUserId}/>
+                        <Box m="2rem 0"/>
+                        {myProfile && <PersonalNavigator/>} {/* Conditionally render PersonalNavigator */}
+                        <Box m="2rem 0"/>
+                    </Box>
                 )}
 
 
                 {isNonMobileScreens && (
-                <Box flexBasis={isNonMobileScreens ? "66%" : undefined} 
-                    paddingRight="2rem"
-                    paddingLeft="2rem"
-                >
-                    <ChatWidget postUserId={postUserId}/>
-                </Box>
+                    <Box flexBasis={isNonMobileScreens ? "66%" : undefined}
+                         paddingRight="2rem"
+                         paddingLeft="2rem"
+                    >
+                        <ChatWidget postUserId={postUserId}/>
+                    </Box>
                 )}
                 {isNonMobileScreens && (
-                <Box flexBasis={isNonMobileScreens ? "26%" : undefined} mr="2rem" >
-                    <UserCard userId={userId}/>
-                </Box>
+                    <Box flexBasis={isNonMobileScreens ? "26%" : undefined} mr="2rem">
+                        <UserCard userId={userId}/>
+                    </Box>
                 )}
             </Box>
         </Box>
     );
-
-
 };
 
 export default MergeDirectMessages;
